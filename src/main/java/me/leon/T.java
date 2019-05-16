1535f20291e0bce481827e0f2264b0529218d56c
Leon <shi406406@qq.com>
2019-05-16 17:40:39 +0800

    d

--- src/main/java/me/leon/T.java
+++ src/main/java/me/leon/T.java
@@ -3,8 +3,11 @@ package me.leon;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import rx.Observable;
-import rx.util.functions.Action1;
+import rx012.Observable;
+import rx012.Observer;
+import rx012.Subscription;
+import rx012.util.functions.Action1;
+import rx012.util.functions.Func1;
 
 public class T {
     private static final Logger logger = LoggerFactory.getLogger(T.class);
@@ -21,7 +24,17 @@ public class T {
     }
 
     public static void hello(String... names) {
-        Observable.toObservable(names).subscribe(new Action1<String>() {
+        Observable.just("hello");
+
+        Observable.toObservable(names)
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String s) {
+                        return " lal  "+s+" lal ";
+                    }
+                })
+
+                .subscribe(new Action1<String>() {
 
             @Override
             public void call(String s) {
